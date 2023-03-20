383bc6f0eba7d040a181c4ad2cb451cef5e19301
Karel63 <76937128+Karel63@users.noreply.github.com>
2023-03-20 18:39:59 +0100

    Update MySQL.php

--- mysqlphp/MySQL.php
+++ mysqlphp/MySQL.php
@@ -1,40 +1,68 @@
 <?php
 require "IDB.php";
-
-//Poznámky k nedostatkům jsou v README.md
+//Tento kód a README.md byl updatován 20.3.2023 díky získáním nových poznatků k vyřešení ze školy.
 
 class MySQL implements IDB{
-  public function connect(string $host = "127.0.0.1", string $username = "root", string $password = "", string $database = "countries"): ?static{
-    $con = mysqli_connect("$host", "$username", "$password", "$database");
-    if ($con -> connect_error){
-      return $con->connect_error;
+  private $con;
+  public function connect(
+    string $host = "", 
+    string $username = "", 
+    string $password = "", 
+    string $database = ""
+  ): ?static{
+    $this->con = mysqli_connect($host, $username, $password, $database);
+    if($this->con === false){
+      echo "Error";
+      exit;
     }
-    return $con;
+    return $this;
   }
   function select(string $query): array{
-    $data = self::connect()->query("SELECT * FROM $query");
+    $data = mysqli_query($this->con, $query);
     $data = mysqli_fetch_all($data, MYSQLI_ASSOC);
     return $data;
   }
   function insert(string $table, array $data): bool{
-    $data = self::connect()->query("INSERT INTO $table(name, capital) VALUES('$data[0]', '$data[1]')");
+    $keys = "";
+    $values = "";
+    $t = 1;
+    foreach($data as $key => $value){
+      if(count($data) == $t){
+        $keys .= $key;
+        $values .= $value;
+        break;
+      }
+      $keys .= $key . ", ";
+      $values .= $value . "', '";
+      $t++;
+    }
+    mysqli_query($this->con, "INSERT INTO $table($keys) VALUES('$values')");
     return true;
   }
   function update(string $table, int $id, array $data): bool{
-    $data = self::connect()->query("UPDATE $table SET name = '$data[0]', capital = '$data[1]'  WHERE id='$id'");
+    $keys = "";
+    $t = 1;
+    foreach($data as $key => $value){
+      if(count($data) == $t){
+        $keys .= $key . " = '" . $value . "'";
+        break;
+      }
+      $keys .= $key . " = '" . $value . "', ";
+      $t++;
+    }
+    mysqli_query($this->con, "UPDATE $table SET $keys WHERE id='$id'");
     return false;
   }
   function delete(string $table, int $id): bool{
-    $data = self::connect()->query("DELETE FROM $table WHERE id='$id'");
+    mysqli_query($this->con, "DELETE FROM $table WHERE id='$id'");
     return true;
   }
 }
 
-$test = new Mysql();
-$test->insert("country", array("Czechia", "Prague"));
-$test->insert("country", array("Slovakia", "Bratislava"));
-$test->insert("country", array("Indonesia", "Jakarta"));
-$test->update("country", 1, array("Czech Republic", "Prague"));
-$test->delete("country", 3);
-$test->connect();
-var_dump($test->select("country"));
+$test = (new Mysql)->connect("127.0.0.1", "root", "", "countries");
+//$test->insert("country", array("name" => "India", "capital" => "New Delhi"));
+//$test->insert("country", array("Slovakia", "Bratislava"));
+//$test->insert("country", array("Indonesia", "Jakarta"));
+//$test->update("country", 1, array("name" => "Hungary", "capital" => "Budapest"));
+//$test->delete("country", 3);
+var_dump($test->select("SELECT * FROM country"));
